a06c81972905e8fef6532cf372af844fd12dee0d
Alan Bertani <alanbertani051@gmail.com>
2024-09-25 15:34:51 -0300

    remove: initial placeholder files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { OpenWeatherService } from './services/openweather.service';
 import { OpenWeatherController } from './controllers/openweather.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, OpenWeatherController],
-  providers: [AppService, OpenWeatherService],
+  controllers: [OpenWeatherController],
+  providers: [OpenWeatherService],
 })
 export class AppModule {}
